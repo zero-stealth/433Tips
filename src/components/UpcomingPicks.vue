9d02755fbbc7efab327455aeba086a6e4a3e522b
zero-stealth <penguincipher@gmail.com>
2023-10-02 04:13:16 +0300

    bug fixes

--- src/components/UpcomingPicks.vue
+++ src/components/UpcomingPicks.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home-main upcoming-p">
+  <div class="home-main">
     <div class="main-h">
       <div class="main-header home-up">
         <div class="header-info home-up">
